8370e2b1c4c64aa0d90ccae7d0b34351ef8722cc
Kirjia <40699415+Kirjia@users.noreply.github.com>
2025-12-26 17:07:13 +0100

    correzione dipendenze

--- R/hello.R
+++ R/hello.R
@@ -1,28 +1,19 @@
-library(readr)
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(janitor)
-library(corrplot)
-library(fastDummies)
-library(hrbrthemes)
-library(heatmaply)
-library(factoextra)
-library(skimr)
-library(psych)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(ggplot2, ggthemes, dplyr, tidyr, readr, janitor, corrplot, fastdummies, heatmaply, factoextra, skimr, psych)
 
 
-dt <- read_tsv(file = 'C:/Users/enzo2/OneDrive/Desktop/openneuro/participants.tsv', na = c("", "NA", "n/a", "N/A"))
+
+
+dt <- read_tsv(file = file.path(getwd(), "/dataset/participants.tsv"), na = c("", "NA", "n/a", "N/A"))
 
 df<-data.frame(dt)
-varianza <- data.frame()
 
 head(df)
 
-# Ti dice quanti NA ci sono in tutto il dataset
+#quanti NA ci sono in tutto il dataset
 sum(is.na(df))
 
-# Ti dice quali colonne hanno NA
+#quali colonne hanno NA
 colSums(is.na(df))
 
 df_stats <- skim(df)
@@ -54,6 +45,10 @@ ggplot(data = df, aes(x = APOE_rs7412, fill = factor(APOE_rs7412))) + geom_bar()
     fill = "Genes"
   )
 
+ggplot(data = df, aes(x= diabetes, fill = factor(diabetes))) + geom_bar() + theme_minimal()
+
+ggplot(data = df, aes(x= hypertension, fill = factor(hypertension))) + geom_bar() + theme_minimal()
+
 ggplot(data = df, aes(y = BDI)) + geom_boxplot() + labs(
   title = "BDI boxplot"
 )
@@ -61,35 +56,35 @@ ggplot(data = df, aes(y = BDI)) + geom_boxplot() + labs(
 #density plot
 df %>% ggplot( aes(x=BDI)) +
   geom_density(fill="#696089", color="#000000", alpha=0.8) +
-  ggtitle("Distribuzione del Beck depression inventory") + theme_ipsum()
+  ggtitle("Distribuzione del Beck depression inventory") + theme_minimal()
 
 df %>% ggplot( aes(x=RPM)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distribuzione di RPM(Raven's progressive Matrix)") + theme_ipsum()
+  ggtitle("Distribuzione di RPM(Raven's progressive Matrix)") + theme_minimal()
 
 df %>% ggplot( aes(x=SES)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distr. rapporto socio economico SES") + theme_ipsum()
+  ggtitle("Distr. rapporto socio economico SES") + theme_minimal()
 
 df %>% ggplot( aes(x=EHI)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distr. EHI grado di quanto una persona sia destrorsa") + theme_ipsum()
+  ggtitle("Distr. EHI grado di quanto una persona sia destrorsa") + theme_minimal()
 
 df %>% ggplot( aes(x=AUDIT)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distr. indice alcoolemico") + theme_ipsum()
+  ggtitle("Distr. indice alcoolemico") + theme_minimal()
 
 df %>% ggplot( aes(x=education)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distr. educazione") + theme_ipsum()
+  ggtitle("Distr. educazione") + theme_minimal()
 
 df %>% ggplot( aes(x=BMI)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distr. massa corporea") + theme_ipsum()
+  ggtitle("Distr. massa corporea") + theme_minimal()
 
 df %>% ggplot( aes(x=learning_deficits)) +
   geom_density(fill="#696089", color="#000000", alpha=0.9) +
-  ggtitle("Distr. indice defict apprendimento") + theme_ipsum()
+  ggtitle("Distr. indice defict apprendimento") + theme_minimal()
 
 
 
@@ -106,7 +101,7 @@ df_long <- pivot_longer(df,
 ggplot(df_long, aes(x = Valore, fill = Variabile)) +
   geom_density(alpha = 0.6, color = NA) + # alpha rende trasparente
   facet_wrap(~ Variabile, ncol = 2) +     # <--- Crea la griglia 2x2
-  theme_ipsum() +
+  theme_minimal() +
   labs(title = "Confronto Densità dei valori NEO", fill = "Variabile")
 
 df <- df %>%
@@ -151,7 +146,7 @@ boxplot(na.omit(df$CVLT_12), xlab="CVLT total hits", col = "green")
 df %>% ggplot(aes(y = CVLT_12))+geom_boxplot( outlier.color = "black", outlier.shape = 16, outlier.size = 2, fill="red", ylab= "count")
 boxplot(df$education)
 
-df %>% ggplot(aes(y = df$RPM))+geom_boxplot( outlier.color = "black", outlier.shape = 16, outlier.size = 2, fill="red")
+df %>% ggplot(aes(y = RPM))+geom_boxplot( outlier.color = "black", outlier.shape = 16, outlier.size = 2, fill="red")
 
 
 
@@ -249,7 +244,7 @@ dataframe_pca <- cbind(dataframe_pca, nuove_dimensioni)
 
 dataframe_pca %>% ggplot( aes(x=Dim_1)) +
   geom_density(fill="#696089", color="#000000", alpha=0.8) +
-  ggtitle("Dim 1") + theme_ipsum()
+  ggtitle("Dim 1") + theme_minimal()
 
 
 ggplot(df, aes(APOE_risk_score, CVLT_13)) +
